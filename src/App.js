fcf4cc9f6b25c107455621d2f3279e4c9820805e
Zaheer Khan Niazi <zaheerniazipak@gmail.com>
2022-10-07 22:25:40 +0500

    Making app realistic by fetching RESTful API

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import React, { Component } from "react";
 import './App.css';
 import SearchBox from "./SearchBox";
 import CardList from './CardList';
-import { robots } from './robots';
 
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    // React LifeCycle
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }))
     }
 
     onSearchChange = (event) => {
@@ -22,13 +28,19 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        return (
-            <div className="tc">
-                <h1 className="f1">Robofriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots} />
-            </div>
-        )
+
+        if (this.state.robots.length === 0) {
+            return <h1>Loading!!!</h1>
+        }
+        else {
+            return (
+                <div className="tc">
+                    <h1 className="f1">Robofriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <CardList robots={filteredRobots} />
+                </div>
+            )
+        }
     }
 }
 
