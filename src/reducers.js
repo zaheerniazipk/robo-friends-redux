24125017a9fc6772fdda7f299d461a2047417574
Zaheer Khan Niazi <zaheerniazipak@gmail.com>
2022-10-11 10:02:56 +0500

    Redux actions and reducers components

---
+++ src/reducers.js
@@ -0,0 +1,15 @@
+import { CHANGE_SEARCH_FIELD } from './constants';
+
+const initialState = {
+    SearchField: ''
+}
+
+// Reducer for Search Robots
+export const searchRobots = (state = initialState, action = {}) => {
+    switch (action.type) {
+        case CHANGE_SEARCH_FIELD:
+            return Object.assign({}, state, { SearchField: action.payload });
+        default:
+            return state;
+    }
+}
